42cc1dabd61da8660ace60c70df63e642ac83273
zwwxxxxx <126754383+ogkunta666@users.noreply.github.com>
2025-10-15 17:52:50 +0200

    uj konyv hozzaadasa

--- App/Services/LibraryManager.php
+++ App/Services/LibraryManager.php
@@ -13,32 +13,11 @@ class LibraryManager
         $this->pdo = $pdo;
     }
 
-    public function addBook($title, $author, $year, $isbn)
+     public function addBook($title, $author, $year, $isbn)
     {
         $stmt = $this->pdo->prepare("INSERT INTO books (title, author, year, isbn) VALUES (?, ?, ?, ?)");
         $stmt->execute([$title, $author, $year, $isbn]);
     }
-
-    public function getBooks()
-    {
-        $stmt = $this->pdo->query("SELECT * FROM books ORDER BY id DESC");
-        $books = [];
-
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $books[] = new Book($row['id'], $row['title'], $row['author'], $row['year'], $row['isbn']);
-        }
-
-        return $books;
-    }
-
-    public function deleteBook($id)
-    {
-        $stmt = $this->pdo->prepare("DELETE FROM books WHERE id = ?");
-        $stmt->execute([$id]);
-    }
-
-    public function clearBooks()
-    {
-        $this->pdo->exec("TRUNCATE TABLE books");
-    }
+   
 }
+
